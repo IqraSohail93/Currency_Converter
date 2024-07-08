6bf34a4104313e19d9f1baead166df5f82343c3c
IqraSohail93 <167875993+IqraSohail93@users.noreply.github.com>
2024-07-08 18:33:21 +0500

    Project: Currency Converter

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+let currencyValue : {[key: string]: number} = {
+    PKR: 278.57,
+    UAE: 3.67,
+    USD: 1
+}
+
+let ans = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        message: "Converting from",
+        choices: ["PKR","UAE","USD"]
+
+},
+{
+    type: "list",
+    name: "to",
+    message: "Converting to",
+    choices: ["PKR","UAE","USD"]
+
+},
+{
+    type: "number",
+    name: "amount",
+    message: "Your amount to",
+
+},
+])
+console.log(currencyValue[ans.to] / currencyValue[ans.from]* ans.amount);
+
